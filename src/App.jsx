9aba13deeaf39b787fa74857a4fe105449cb1b80
BaniHillabi <ibanihillabi2007@gmail.com>
2024-01-14 23:18:57 +0700

    1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HitungBMI from './components/hitungBmi';
+
+const App = () => {
+  return (
+    <div>
+      <HitungBMI/>
+    </div>
+  );
+};
+
+export default App;
